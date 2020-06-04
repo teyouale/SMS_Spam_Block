e70bae65420c13044ad520773af766a3ef24c615
eyouale <teyouale@gmail.com>
2020-06-04 13:18:14 +0300

    Initializing Git Repo and Adding Git Ignored

---
+++ app/src/main/java/com/teyouale/smsspamblock/MainActivity.java
@@ -0,0 +1,14 @@
+package com.teyouale.smsspamblock;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
